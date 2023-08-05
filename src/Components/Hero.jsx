602003b8a62f0ff0df049369f8fc77f02e56605d
Mohammad <mehmandoost2@gmail.com>
2023-08-05 13:49:34 +0430

    Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -20,17 +20,52 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Left = styled.div``;
-
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const Title = styled.h1`
+  font-weight: 600;
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 5px;
+`;
+const Subtitle = styled.h2``;
+const Desc = styled.p``;
+const Button = styled.button``;
+const Right = styled.div`
+  flex: 3;
+`;
+const Img = styled.img``;
 
 export const Hero = () => {
   return (
     <Section>
       <Navbar />
       <Container>
-        <Left>Left</Left>
-        <Right>Right</Right>
+        <Left>
+          <Title>Think. Think. Solve.</Title>
+          <WhatWeDo>
+            <Line src="../../public/img/line.png" />
+            <Subtitle>What We Do?</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we enjoy creating delightful, human-centered digital expriences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/* 3dModel */}
+          <Img src="../../public/img/moon.png" />
+        </Right>
       </Container>
     </Section>
   );
